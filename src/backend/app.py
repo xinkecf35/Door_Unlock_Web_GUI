7cb39d52bbe62ec0a0eb37ea2c9057eea693f7e2
Xinke Chen <xinkecf35@gmail.com>
2019-09-04 14:07:25 -0400

    Updated dependencies

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,8 +1,12 @@
 from flask import Flask, Response, jsonify, url_for
 from flask_restful import Resource, Api
 
+import confuse
+
 app = Flask(__DoorUnlockAPI__)
 api = Api(app)
+config = confuse.Configuration('door-unlock-api')
+db = d
 
 
 @app.route('/')
