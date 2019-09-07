aa4cec3efe624b6ce7ff04639927acf729259447
Xinke Chen <xinkecf35@gmail.com>
2019-09-06 21:09:52 -0400

    Settled on configuration method

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,15 +1,26 @@
 from flask import Flask, Response, jsonify, url_for
 from flask_restful import Resource, Api
+from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
+import os
+import yaml
 
-import confuse
+# load config from yaml file
+with open('config.yaml', 'r') as configFile:
+    config = yaml.safe_load(configFile)
 
-app = Flask(__DoorUnlockAPI__)
+# Global app variables setup
+app = Flask(__name__)
 api = Api(app)
-config = confuse.Configuration('door-unlock-api')
-db = d
 
 
-@app.route('/')
+# Database/SQLAlchemy Setup
+currentDir = os.getcwd()
+dbName = config['SQLITE_DB_NAME']
+dbURI = f'sqlite:////{currentDir}/database/{dbName}'
+app.config['SQLALCHEMY_DATABASE_URI'] = dbURI
+
+
 def hello():
     return jsonify('Hello from Door Unlocker API')
 
