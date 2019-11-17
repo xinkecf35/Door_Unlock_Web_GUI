5a01079085e4fd43def5f7b1f0b5cc1468a39279
Xinke Chen <xinkecf35@gmail.com>
2019-11-17 03:57:06 -0500

    Change testInsert to use generated data and compare that.

--- src/backend/tests/test_database.py
+++ src/backend/tests/test_database.py
@@ -1,9 +1,17 @@
 import pytest
+import names
+from random import randint
 from src.database.Person import Person
 from src.database.Role import Role
 
+
 @pytest.mark.usefixtures('db')
 class TestPerson:
+    def generateName(self):
+        firstName = names.get_first_name()
+        lastName = names.get_last_name()
+        username = ''.join([firstName, lastName, str(randint(1, 1000))])
+        return (firstName, lastName, username)
 
     def testInsert(self, db):
         role1 = Role(name='member',
@@ -11,16 +19,18 @@ class TestPerson:
                      canManage=0,
                      canAccessHistory=0)
         db.session.add(role1)
-        person1 = Person(firstName='John',
-                         lastName='Doe',
-                         username='johndoe001')
-        person2 = Person(firstName='Jane',
-                         lastName='Doe',
-                         username='janedoe001')
-        assert person1.firstName == 'John'
-        assert person2.firstName == 'Jane'
-        db.session.add(person1)
-        db.session.add(person2)
-        db.session.commit()
-        query1 = Person.query.filter_by(firstName='John').first()
-        print(query1)
+
+        persons = []
+        names = []
+        for _ in range(10):
+            data = self.generateName()
+            names.append(data)
+            persons.append(Person(firstName=data[0],
+                                  lastName=data[1],
+                                  username=data[2]))
+        for person in persons:
+            db.session.add(person)
+        for name in names:
+            testUsername = name[2]
+            queryPerson = Person.query.filter_by(username=testUsername).first()
+            assert queryPerson.username == testUsername
